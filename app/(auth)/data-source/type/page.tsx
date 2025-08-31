671dbdbf066dc965745fe14e062184364dd3cc5f
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-08-31 13:03:56 +0530

    link tag added

--- app/(auth)/data-source/type/page.tsx
+++ app/(auth)/data-source/type/page.tsx
@@ -1,6 +1,7 @@
 import Csv from "@/components/data-source/csv";
 import Postgres from "@/components/data-source/postgres";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 import { IoIosArrowBack } from "react-icons/io";
 
@@ -9,13 +10,15 @@ const DataSourceTypePage = () => {
     <div className="flex flex-col mx-auto">
       <div className="w-[800px] flex flex-col gap-8 py-4">
         <div className="flex justify-between items-end">
-          <Button
-            variant={"ghost"}
-            className="font-bold text-md flex justify-start !p-0 !m-0"
-          >
-            <IoIosArrowBack />
-            Select source type
-          </Button>
+          <Link href={"/data-source"}>
+            <Button
+              variant={"ghost"}
+              className="font-bold text-md flex justify-start !p-0 !m-0"
+            >
+              <IoIosArrowBack />
+              Select source type
+            </Button>
+          </Link>
         </div>
         <div className="flex flex-col gap-4">
           <div className="font-bold">Database</div>
