35f369acd55f89786b6eee532c099a5ff23c944a
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-10-07 12:31:03 +0530

    next app added

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,103 @@
-import { Button } from "@/components/ui/button";
-import { FaLinkedin } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import Navbar from "@/components/landing-page/navbar";
-import Home from "@/components/landing-page/home";
-import Feature from "@/components/landing-page/feature";
-import FAQ from "@/components/landing-page/faq";
-import Link from "next/link";
-import LogoSVG from "@/components/icons/LogoSVG";
+import Image from "next/image";
 
-export default function LandingPage() {
+export default function Home() {
   return (
-    <div className="flex flex-col max-w-[1400px] mx-auto gap-8 md:gap-38 px-4 sm:px-6 lg:px-8">
-      <div>
-        <Navbar />
-        <Home />
-      </div>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
 
-      <div className="flex flex-col justify-center items-center gap-6 md:gap-10">
-        <div className="bg-gray-50 rounded-2xl px-3 py-2 border text-sm font-semibold">
-          How it works
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-        <div className="font-bold text-2xl sm:text-3xl md:text-4xl flex flex-col justify-center items-center text-center">
-          <div>Make data analysis self-service</div>
-        </div>
-      </div>
-
-      <Feature />
-      <FAQ />
-
-      <div className="flex flex-col justify-center items-center gap-5">
-        <div className="text-white rounded-4xl w-full max-w-[900px] py-10 md:py-20 bg-black flex flex-col justify-center items-center gap-5 px-6 sm:px-8">
-          <div className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center">
-            Talk to your data today
-          </div>
-          <div className="text-sm text-muted-foreground font-bold text-center">
-            Make better data-driven decisions 10x faster with a fraction of the
-            cost
-          </div>
-          <Link href={"/login"}>
-            <Button className="text-black" variant={"outline"}>
-              Get started
-            </Button>
-          </Link>
-        </div>
-      </div>
-
-      <div className="flex flex-col justify-center items-center">
-        <div className="w-full max-w-[900px] border-t flex flex-col gap-5 py-10">
-          <div className="flex gap-1 items-center">
-            <LogoSVG height="40" width="40" />
-
-            <div className="font-bold text-xl sm:text-2xl">Zepr</div>
-          </div>
-          <div>Copyright @ 2025 Zepr.live</div>
-          <div className="flex gap-3">
-            <a
-              href="https://linkedin.com/in/rajeshcharhajari"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button className="bg-gray-100">
-                <FaLinkedin color="black" />
-              </Button>
-            </a>
-            <a
-              href="https://x.com/rajeshwebdev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button className="bg-gray-100">
-                <FaSquareXTwitter color="black" />
-              </Button>
-            </a>
-          </div>
-        </div>
-      </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
