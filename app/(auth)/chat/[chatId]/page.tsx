b11bace46874f09f64e9e81fbeeeb9fe985e705f
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-10-13 14:34:16 +0530

    chat page css

--- app/(auth)/chat/[chatId]/page.tsx
+++ app/(auth)/chat/[chatId]/page.tsx
@@ -1,18 +1,49 @@
-import { SendHorizontal } from "lucide-react";
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { SendHorizontal, X } from "lucide-react";
+import React, { useState } from "react";
 
 const ChatIdPage = () => {
+  const [rightPanel, setRightPanel] = useState(true);
   return (
     <div className="flex h-full">
-      <div className="w-1/3 flex flex-col border-r">
+      <div
+        className={cn(
+          "w-1/3 flex flex-col border-r",
+          !rightPanel && "w-full border-none"
+        )}
+      >
         <div className="px-2 py-1">Title</div>
-        <div className="flex-1 overflow-y-auto p-2">
-          {Array.from({ length: 3 }).map((_, i) => (
-            <div key={i}>hi {i + 1}</div>
+        <div
+          className={cn(
+            "flex-1 overflow-y-auto p-2 flex flex-col gap-2 ",
+            !rightPanel && "px-[25%]"
+          )}
+        >
+          {Array.from({ length: 5 }).map((_, i) => (
+            <div key={i} className="flex flex-col">
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Corrupti unde facere, numquam exercitationem, mollitia cumque
+                beatae nobis voluptas iure facilis quidem impedit dolorem. Totam
+                laudantium quis necessitatibus eum pariatur reiciendis. {i + 1}
+              </div>
+              <div>
+                <Button variant={"outline"} onClick={() => setRightPanel(true)}>
+                  Show report
+                </Button>
+              </div>
+            </div>
           ))}
         </div>
 
-        <div className="border p-2 rounded-xl m-2 bg-white shadow flex flex-col gap-3">
+        <div
+          className={cn(
+            "border p-2 rounded-xl m-2 bg-white flex flex-col gap-3",
+            !rightPanel && "mx-[25%]"
+          )}
+        >
           <input
             type="text"
             className="outline-none"
@@ -25,14 +56,19 @@ const ChatIdPage = () => {
         </div>
       </div>
 
-      <div className="flex-1 flex flex-col bg-white rounded-r p-2">
-        <div>Title</div>
-        <div className="overflow-y-auto">
-          {Array.from({ length: 4 }).map((_, i) => (
-            <div key={i}>hi {i + 1}</div>
-          ))}
+      {rightPanel && (
+        <div className="flex-1 flex flex-col bg-white rounded-r p-2">
+          <div className="flex justify-between">
+            <div>Title</div>
+            <X size={15} onClick={() => setRightPanel(false)} />
+          </div>
+          <div className="overflow-y-auto">
+            {Array.from({ length: 40 }).map((_, i) => (
+              <div key={i}>hi {i + 1}</div>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
