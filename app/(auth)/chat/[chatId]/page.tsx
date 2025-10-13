b838c8f6ad1d3be501067c49d4bdc46979b392b7
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-10-13 13:55:04 +0530

    chat page css fix

--- app/(auth)/chat/[chatId]/page.tsx
+++ app/(auth)/chat/[chatId]/page.tsx
@@ -7,22 +7,20 @@ const ChatIdPage = () => {
       <div className="w-1/3 flex flex-col border-r">
         <div className="px-2 py-1">Title</div>
         <div className="flex-1 overflow-y-auto p-2">
-          {Array.from({ length: 30 }).map((_, i) => (
+          {Array.from({ length: 3 }).map((_, i) => (
             <div key={i}>hi {i + 1}</div>
           ))}
         </div>
 
-        <div className="border p-2 rounded-xl m-2 bg-white">
-          <div className="space-y-2">
-            <input
-              type="text"
-              className="outline-none"
-              placeholder="Ask a question"
-            />
-            <div className="flex justify-between">
-              <div className="border p-1 rounded-lg">Netflix.csv</div>
-              <SendHorizontal size={20} color="gray" />
-            </div>
+        <div className="border p-2 rounded-xl m-2 bg-white shadow flex flex-col gap-3">
+          <input
+            type="text"
+            className="outline-none"
+            placeholder="Ask a question"
+          />
+          <div className="flex justify-between">
+            <div className="border p-1 rounded-lg text-xs">Netflix.csv</div>
+            <SendHorizontal size={20} color="gray" />
           </div>
         </div>
       </div>
@@ -30,7 +28,7 @@ const ChatIdPage = () => {
       <div className="flex-1 flex flex-col bg-white rounded-r p-2">
         <div>Title</div>
         <div className="overflow-y-auto">
-          {Array.from({ length: 40 }).map((_, i) => (
+          {Array.from({ length: 4 }).map((_, i) => (
             <div key={i}>hi {i + 1}</div>
           ))}
         </div>
